23201c545e63d682896d3d9d5470ee2b57e48434
02Abhay2002 <156237222+02Abhay2002@users.noreply.github.com>
2024-03-26 17:44:48 +0530

    Initial commit

---
+++ emoji.js
@@ -0,0 +1,5 @@
+const emoji = document.querySelector(".emoji");
+const emojis = ["😒","😁","😆","😅","😉","😊","😋","☺️","🥰","🙂","🤗","🤩","🤔","😖","😞","😲","🤑","🤯","🤐","🤫","🥳"];
+emoji.addEventListener("mouseover", () => {
+    emoji.innerText = emojis[Math.floor(Math.random()*emojis.length)];
+});
